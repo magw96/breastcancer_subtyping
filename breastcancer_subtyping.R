e4e9da423ab6cb9e1926b5dfa60e9909a3675063
Miguel Angel Gonzalez-Woge <73000825+magw96@users.noreply.github.com>
2021-05-04 10:20:47 -0500

    Update breastcancer_subtyping.R

--- breastcancer_subtyping.R
+++ breastcancer_subtyping.R
@@ -5,22 +5,22 @@ library(tidyverse)
 
 # --- Working with salmon quant.sf files
 # --- Import Salmon counts (using countsfromAbundance = LengthScaledTPM)
-txi_f <- tximport(files, type="salmon", tx2gene=t2g[,c("ensembl_transcript_id", "ensembl_gene_id")], 
+txi <- tximport(files, type="salmon", tx2gene=t2g[,c("ensembl_transcript_id", "ensembl_gene_id")], 
                   countsFromAbundance="lengthScaledTPM", ignoreTxVersion = T)
 
 # --- Obtain Salmon abundance counts (length-scaled TPM)
 # --- log10-scaled; + 1 to avoid problems with values equal to 0
-bc_subtype <- log10(txi_f$abundance + 1)
+bc_subtype <- log10(txi$abundance + 1)
 
-# --- Assign column names to your samples
+# --- Assign column names to your samples (optional)
 colnames(bc_subtype) <- c('AU565(6)', 'BT474', 'BT474(1)', 'BT483', 'BT549', 'CAL120', 'CAL851', 'CAMA1', 'CL184A1(3)', 'CL184B5(2)', 'EFM192A', 'EVSAT', 'HCC1143', 'HCC1395', 'HCC1419', 'HCC1428', 'HCC1500', 'HCC1569(1)', 'HCC1599', 'HCC1806', 'HCC1937', 'HCC1954', 'HCC202', 'HCC2185', 'HCC2218', 'HCC712', 'HME1', 'JIMT1', 'MCF10A', 'MCF10A1', 'MCF10A2', 'MCF10A3', 'MCF10A4', 'MCF12A', 'MCF7', 'MDAMB134', 'MDAMB175VII', 'MDAMB330', 'MDAMB361', 'MDAMB361(1)', 'MDAMB415', 'SKBR3', 'SKBR3(9)', 'SUM102', 'SUM1315', 'T47D', 'UACC8126', 'ZR751(4)', 'ZR7530(1)')
 
 # --- Create the annotation file which will be used as input for genefu()
 bc_annot <- as.data.frame(rownames(bc_subtype))
-colnames(bc_annot) <- "ensembl_gene_id"
+colnames(bc_annot) <- "ensembl_gene_id" # Optional, merge() can handle different colnames (specify by.x and by.y)
 
 # --- Merge annotation file with gene_biotype (which contains c("ensembl_gene_id", "gene_biotype" & "entrez_gene_id")
-# --- gene_biotype was previously obtained; annotated using biomaRt
+# --- An object similar to gene_biotype should be previously obtained; I used biomaRt for annotation
 bc_annot <- merge(bc_annot, gene_biotype, by = "ensembl_gene_id", no.dups = T)
 
 # --- Remove duplicated EntrezGene IDs (as they can cause problems when used for identifying rows or columns)
@@ -65,7 +65,7 @@ table(ssp.2003.subtypes$subtype)
 # Basal   Her2   LumB   LumA Normal 
 # 19      2     10     15      3 
 
-table(ssp.subtypes$subtype, matrix$Subtipo)
+table(ssp.subtypes$subtype, matrix$Subtipo) # --- A cross-table can be built in order to see how subtyping matches (or mismatches) your samples
 #         NM HER2 LA LB TNBC
 # Basal   5    3  0  0    9
 # Her2    0    6  0  3    0
